d130d5017930d4ea7e52254906cf270c109ebea0
RatonLaveur <AjaxRid@yandex.ru>
2024-05-10 11:45:42 +0300

    added_docker#8

--- server/src/main/java/ru/practicum/shareit/item/model/Item.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.io.Serializable;
 
 @Table(name = "items", schema = "public")
 @Data
